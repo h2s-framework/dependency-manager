96ab33546d4beaddfcd47b61304f9083ef98f77c
Siarko <labroqu@gmail.com>
2025-05-02 21:38:26 +0200

    Make auto generated proxies extend base class

--- src/Generator/Developer/Proxy/ProxyGenerator.php
+++ src/Generator/Developer/Proxy/ProxyGenerator.php
@@ -5,6 +5,7 @@ namespace Siarko\DependencyManager\Generator\Developer\Proxy;
 use Nette\PhpGenerator\ClassType;
 use Nette\PhpGenerator\Method;
 use Nette\PhpGenerator\PhpFile;
+use Siarko\Api\Factory\ObjectCreatorInterface;
 use Siarko\DependencyManager\Generator\IGenerator;
 use Siarko\Utils\Code\ClassStructureProvider;
 use Siarko\Utils\Code\MethodStructure;
@@ -18,6 +19,12 @@ class ProxyGenerator implements IGenerator
 
     public const SUFFIX = '\\Proxy';
     const CONSTRUCTOR_NAME = '__construct';
+
+    private const FIELD_TYPENAME = '__PROXY_TARGET_TYPE_NAME';
+    private const FIELD_INSTANCE = '__proxyInstance';
+    private const FIELD_OBJECT_CREATOR = '__proxyObjectCreator';
+
+    private const METHOD_PROXY = '__proxyGetSubject';
 
     /**
      * @param ClassStructureProvider $classStructureProvider
@@ -47,7 +54,6 @@ class ProxyGenerator implements IGenerator
         $baseClassName = substr($fullClassName, 0, -strlen(self::SUFFIX));
         $file = new PhpFile();
         $class = $file->addClass($fullClassName);
-        $class->setExtends(AbstractProxy::class);
         $this->generateClass($class, $baseClassName);
         return $file;
     }
@@ -58,16 +64,33 @@ class ProxyGenerator implements IGenerator
      * @return void
      * @throws \ReflectionException
      */
-    private function generateClass($class, $baseClassName): void
+    private function generateClass(ClassType $class, $baseClassName): void
     {
+        $class->setExtends($baseClassName);
         $structure = $this->classStructureProvider->get($baseClassName);
         $this->storeBaseTypeName($class, $baseClassName);
+        $this->createClassFields($class, $baseClassName);
+        $this->generateProxyMethod($class, $structure);
         foreach ($structure->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
             if ($method->getName() == self::CONSTRUCTOR_NAME) {
-                continue;
+                $this->generateConstructor($class, $method);
+            }else{
+                $this->generateMethod($class, $method);
             }
-            $this->generateMethod($class, $method);
         }
+    }
+
+    private function generateConstructor(ClassType $class, MethodStructure $method): void
+    {
+        $newMethod = $class->addMethod($method->getName());
+        $newMethod->setPublic();
+        $newMethod->setComment($this->getDocBlock($method));
+
+        $objectCreatorParam = $newMethod->addParameter(self::FIELD_OBJECT_CREATOR);
+        $objectCreatorParam->setType(ObjectCreatorInterface::class);
+
+        $parentCall = '$this->'.self::FIELD_OBJECT_CREATOR.' = $'.self::FIELD_OBJECT_CREATOR.';';
+        $newMethod->setBody($parentCall);
     }
 
     /**
@@ -84,8 +107,7 @@ class ProxyGenerator implements IGenerator
         $returnType = $method->getNativeReflection()->getReturnType();
         $returnsData = ($returnType && $returnType->getName() !== 'void');
 
-        $pluginExecutionCode = ($returnsData ? 'return ' : '') .
-            '$this->__getSubject()->' . $method->getName() . '(' . $params . ');';
+        $pluginExecutionCode = ($returnsData ? 'return ' : '') . '$this->'.self::METHOD_PROXY.'()->' . $method->getName() . '(' . $params . ');';
         $newMethod->setBody($pluginExecutionCode);
     }
 
@@ -120,6 +142,7 @@ class ProxyGenerator implements IGenerator
         if ($parameter->isDefaultValueAvailable()) {
             $newParam->setDefaultValue($parameter->getDefaultValue());
         }
+        $newParam->setNullable($parameter->allowsNull());
         $paramType = $parameter->getType();
         if ($paramType) {
             $newParam->setType($paramType->getName());
@@ -140,6 +163,19 @@ class ProxyGenerator implements IGenerator
         return $result;
     }
 
+    private function generateProxyMethod(ClassType $class, \Siarko\Utils\Code\ClassStructure $structure): void
+    {
+        $proxyMethodBody = 'if( !$this->'.self::FIELD_INSTANCE." ){\n\t\$this->".
+            self::FIELD_INSTANCE.' = $this->'.self::FIELD_OBJECT_CREATOR. "->createObject(self::" .
+            self::FIELD_TYPENAME. ");\n}\n";
+        $proxyMethodBody .= 'return $this->'.self::FIELD_INSTANCE.';';
+        $method = $class->addMethod(self::METHOD_PROXY);
+        $method->setPrivate();
+        $method->setReturnType($structure->getName());
+        $method->setBody($proxyMethodBody);
+        $method->addComment('Returns instance of the proxied object');
+    }
+
     /**
      * @param ClassType $class
      * @param string $baseClassName
@@ -147,10 +183,21 @@ class ProxyGenerator implements IGenerator
      */
     private function storeBaseTypeName(ClassType $class, string $baseClassName): void
     {
-        $property = $class->addProperty(AbstractProxy::TYPENAME_PROPERTY);
-        $property->setProtected();
-        $property->setStatic();
-        $property->setType('string');
-        $property->setValue($baseClassName);
+        $class->addConstant(self::FIELD_TYPENAME, $baseClassName)
+            ->addComment('Stores base class name for this proxy');
     }
+
+    private function createClassFields(ClassType $class, string $baseClassName): void
+    {
+        $class->addProperty(self::FIELD_OBJECT_CREATOR)
+            ->setPrivate()
+            ->setType(ObjectCreatorInterface::class)
+            ->addComment('Stores object creator for this proxy');
+        $class->addProperty(self::FIELD_INSTANCE)
+            ->setPrivate()
+            ->setType($baseClassName)
+            ->setValue(null)
+            ->addComment('Stores instance of the proxied object');
+    }
+
 }
